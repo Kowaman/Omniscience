c69f640bba88aedc185c0fe66ee156d183ecdeff
501warhead <501warhead@gmail.com>
2019-03-25 13:55:50 -0400

    Log BlockDispenseEvent results

--- Omniscience-Core/src/main/java/net/lordofthecraft/omniscience/listener/item/EventDropListener.java
+++ Omniscience-Core/src/main/java/net/lordofthecraft/omniscience/listener/item/EventDropListener.java
@@ -1,14 +1,14 @@
 package net.lordofthecraft.omniscience.listener.item;
 
 import com.google.common.collect.ImmutableList;
-import net.lordofthecraft.omniscience.Omniscience;
 import net.lordofthecraft.omniscience.api.entry.OEntry;
 import net.lordofthecraft.omniscience.listener.OmniListener;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
+import org.bukkit.event.block.BlockDispenseEvent;
 import org.bukkit.event.entity.EntityDropItemEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
-import org.bukkit.metadata.FixedMetadataValue;
 
 public class EventDropListener extends OmniListener {
 
@@ -16,17 +16,20 @@ public class EventDropListener extends OmniListener {
         super(ImmutableList.of("drop"));
     }
 
-    @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
     public void onPlayerDropItem(PlayerDropItemEvent event) {
         OEntry.create().source(event.getPlayer()).dropped(event.getItemDrop()).save();
-        event.getItemDrop().setMetadata("omniTracked", new FixedMetadataValue(Omniscience.getPluginInstance(), "tracked"));
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
     public void onEntityDropItem(EntityDropItemEvent event) {
-        if (!event.getItemDrop().hasMetadata("omniTracked")) {
+        if (!(event.getEntity() instanceof Player)) {
             OEntry.create().source(event.getEntity()).dropped(event.getItemDrop()).save();
-            event.getItemDrop().setMetadata("omniTracked", new FixedMetadataValue(Omniscience.getPluginInstance(), "tracked"));
         }
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+    public void onBlockDispense(BlockDispenseEvent event) {
+        OEntry.create().source(event.getBlock()).droppedItem(event.getItem(), event.getBlock().getLocation()).save();
     }
 }
